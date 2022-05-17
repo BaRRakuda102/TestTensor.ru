c7b4310218cb124ae5c7936c29a329f1c732406a
Domeshek_Vladislav <domesh.s@yandex.ru>
2022-05-17 14:51:41 +0500

    add capabilities

--- src/test/java/helpers/TestBase.java
+++ src/test/java/helpers/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -12,6 +13,11 @@ public class TestBase {
     static void BeforeAll() {
         Configuration.browserSize = "1980x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
     @AfterEach
     void addAttachments() {
